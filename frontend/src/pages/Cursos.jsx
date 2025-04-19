3004bccd31606904e4f57b51c9c16bd863b0bed0
victor-trindade-santos <victortrindade.free@gmail.com>
2025-04-19 20:25:12 -0300

    Adicionando as atualizações de Magia, Astrologias, Cursos, Numerologia, Tarot

--- frontend/src/pages/Cursos.jsx
+++ frontend/src/pages/Cursos.jsx
@@ -1,49 +1,61 @@
 import Card from '../components/Card';
+import MagiaIMG from '../img/magia.png';
+import Header from '../components/Header';
+import articleImage from '../img/artigofoto.jpg';
 import courseImage1 from '../img/card_tipo1.jpg';
 import courseImage2 from '../img/Card-tipo2(fundo).jpg';
+import styles from '../css/Magia.module.css';
 
 function Cursos() {
 
     const courses = [
-        { 
-            image: courseImage1, 
-            title: "Curso 1", 
-            description: "Descrição do curso 1" 
+        {
+            image: courseImage1,
+            title: "Curso 1",
+            description: "Descrição do curso 1"
         },
-        { 
-            image: courseImage2, 
-            title: "Curso 2", 
-            description: "Descrição do curso 2" 
+        {
+            image: courseImage2,
+            title: "Curso 2",
+            description: "Descrição do curso 2"
         },
-        { 
-            image: courseImage1, 
-            title: "Curso 3", 
-            description: "Descrição do curso 3" 
+        {
+            image: courseImage1,
+            title: "Curso 3",
+            description: "Descrição do curso 3"
         },
-        { 
-            image: courseImage2, 
-            title: "Curso 4", 
-            description: "Descrição do curso 4" 
+        {
+            image: courseImage2,
+            title: "Curso 4",
+            description: "Descrição do curso 4"
         }
     ]
 
-    return(
+    return (
         <>
-          <h1> Cursos </h1>
-          <div className = "container">
-                <div className = "row g-4">
-                    {courses.map((course, index) => (
-                        <div key = {index} className = "col-12 col-sm-6 col-md-4">
-                            <Card
-                                image = {course.image}
-                                title = {course.title}
-                                description= {course.description}
-                                link = {course.link}
-                            />
-                        </div>
-                    ))}
+            <div>
+                <Header
+                    title="Cursos"
+                    backgroundImage={MagiaIMG}
+                    description="Conhecimentos místicos que transcendem o comum."
+                />
+                <div className={styles.sectionDivider}></div>
+                <div className="container">
+                    <div className="row g-4">
+                        {courses.map((course, index) => (
+                            <div key={index} className="col-12 col-sm-6 col-md-4">
+                                <Card
+                                    image={course.image}
+                                    title={course.title}
+                                    description={course.description}
+                                    link={course.link}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
+
         </>
     )
 }
--- frontend/src/pages/Numerologia.jsx
+++ frontend/src/pages/Numerologia.jsx
@@ -1,8 +1,46 @@
 import Card from '../components/Card';
 import courseImage1 from '../img/card_tipo1.jpg';
 import courseImage2 from '../img/Card-tipo2(fundo).jpg';
+import React from 'react';
+import Header from '../components/Header';
+import MagiaIMG from '../img/magia.png';
+import articleImage from '../img/artigofoto.jpg';
+import styles from '../css/Magia.module.css';
 
 function Numerologia() {
+
+    const articles = [
+        {
+            image: articleImage,
+            title: "Introdução à Magia", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus consectetur consequatur similique totam autem alias. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia mollitia nisi dolores sequi accusamus dolor totam aliquid voluptate nobis. Sapiente nemo ullam modi facere nisi quisquam voluptatum illum neque non."
+        },
+        {
+            image: articleImage,
+            title: "Como lançar feitiços", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae "
+        },
+        {
+            image: articleImage,
+            title: "A história da magia", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus"
+        },
+        {
+            image: articleImage,
+            title: "A história da magia", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus"
+        },
+        {
+            image: articleImage,
+            title: "A história da magia", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus"
+        },
+        {
+            image: articleImage,
+            title: "A história da magia", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus"
+        }
+    ];
 
     const courses = [
         { 
@@ -24,27 +62,59 @@ function Numerologia() {
             image: courseImage2, 
             title: "Curso 4", 
             description: "Descrição do curso 4" 
+        },
+        { 
+            image: courseImage1, 
+            title: "Curso 4", 
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum quae laboriosam temporibus" 
         }
-    ]
+    ];
 
     return(
-        <>
-          <h1> Numerologia </h1>
-          <div className = "container">
-                <div className = "row g-4">
-                    {courses.map((course, index) => (
-                        <div key = {index} className = "col-12 col-sm-6 col-md-4">
-                            <Card
-                                image = {course.image}
-                                title = {course.title}
-                                description= {course.description}
-                                link = {course.link}
+        <div>
+            <Header 
+                title = "Numerologia"
+                backgroundImage= {MagiaIMG}
+                description= "Explore o mundo da numerologia"
+            />
+            <section className = {styles.articlesSection}>
+                <h2 className = {styles.articlesTitle} >Artigos recentes</h2>
+                    <div className = {styles.articlesCards}>
+                        {articles.map((article, index) => (
+                            <Card 
+                                key={index}
+                                image={article.image}
+                                title={article.title}
+                                description={article.description}
+                                link={article.link}
                             />
-                        </div>
-                    ))}
+                        ))}
+                    </div>
+            </section>
+
+            <div className={styles.sectionDivider}></div>
+
+            <section className = {styles.coursesSection}>
+                <h2 className = {styles.coursesTitle}>Confira meus Cursos</h2>
+                <p className = {styles.coursesQuote}>
+                    <span>"Conhecimentos místicos que transcendem o comum."</span>
+                </p>
+                <div className = "container">
+                    <div className = "row g-4">
+                        {courses.map((course, index) => (
+                            <div key = {index} className = "col-12 col-sm-6 col-md-4">
+                                <Card
+                                    image = {course.image}
+                                    title = {course.title}
+                                    description= {course.description}
+                                    link = {course.link}
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </>
+            </section>
+        </div>
     )
 }
 
