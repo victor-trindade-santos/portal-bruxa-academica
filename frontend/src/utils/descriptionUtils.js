098cdd215f55733b6ed3cea84626bed2bf407ade
Lucas Aguena Gatto <lucas.gatto@fatec.sp.gov.br>
2025-05-08 06:57:07 -0300

    description
    arrumado erro do description

--- frontend/src/utils/descriptionUtils.js
+++ frontend/src/utils/descriptionUtils.js
@@ -1,5 +1,5 @@
 export const truncateDescription = (text, length) => {
-    if (text.length <= length) return text;
-    return text.substring(0, length) + '...';
-  };
-  
+  if (!text || typeof text !== 'string') return '';
+  if (text.length <= length) return text;
+  return text.substring(0, length) + '...';
+};
