ab25775dc6e6da21223a725f1b232df627079e39
Luna Leão <lunaleaodemariasona@gmail.com>
2025-06-11 23:20:41 -0300

    teste de render

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('https://portal-bruxa-academica.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
